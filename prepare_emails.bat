8752866a1d5349f83d29d46ac457736fd08df068
Chris Brown <chriseisbrown@gmail.com>
2015-01-14 20:08:00 +0000

    set path in scripts and code in write to logs

--- prepare_emails.bat
+++ prepare_emails.bat
@@ -1,1 +1,2 @@
-python src\data_loader.py --infolder input-data --outfolder output-data
+set PATH=%PATH%;C:\Python27\;C:\Python27\Scripts
+python src\data_loader.py --infolder input-data --outfolder output-data > prepare_emails_log.log
--- send_emails.bat
+++ send_emails.bat
@@ -1,1 +1,2 @@
-python src\data_loader.py --infolder input-data --send 
+set PATH=%PATH%;C:\Python27\;C:\Python27\Scripts
+python src\data_loader.py --infolder input-data --send > send_emails_log.log
